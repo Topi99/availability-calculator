ece0105335d125527981f66f183bdf3d6c748747
Topiltzin Hernández Mares <topiltzin.hernandez@wizeline.com>
2020-11-09 09:02:01 -0600

    Add logic to return values for available times

--- src/clj/availability/controllers/availability.clj
+++ src/clj/availability/controllers/availability.clj
@@ -4,16 +4,29 @@
 (defn parse-int [s]
   (Integer. (re-find  #"\d+" s )))
 
-;; simple map implementation
+(defn round
+  "Round a double to the given precision (number of significant digits)"
+  [precision d]
+  (let [factor (Math/pow 10 precision)]
+    (/ (Math/round (* d factor)) factor)))
+
 ;; my-map: callable, seq -> seq
 (defn my-map [func list]
+  "Simple map implementation"
   (if (empty? list)
     ()
     (cons (func (first list)) (my-map func (rest list)))))
 
-;; helper function to make tail recursion
+;; my-append: list, any -> list
+(defn my-append [list el]
+  "Returns new list with the given element added at last of original list"
+  (if (empty? list) 
+    (cons el [])
+    (cons (first list) (my-append (rest list) el))))
+
 ;; helper-to-decimal-hour: seq, string string boolean -> seq
 (defn helper-to-decimal-hour [inp-hour hours minutes now-minutes]
+  "Helper function to make tail recursion"
   (if (empty? inp-hour)
     (+ (parse-int hours) (/ (parse-int minutes) 60)) ;; sum minutes and hours
     (if (= (first inp-hour) \:) ;; if found ":", then look for munutes
@@ -25,10 +38,33 @@
           (rest inp-hour) (str hours (first inp-hour)) minutes now-minutes)))))
 
 (defn to-decimal-hour [hour]
+  "Returns a given hour in HH:MM string format to its decimal equivalent"
   (helper-to-decimal-hour (seq hour) "" "" false))
 
-;; main function to get availability
+(defn helper-get-available [start end calendar result]
+  (cond
+    (and (not (empty? (first calendar))) (empty? (rest calendar))) ;; last event
+      (my-append result [(first (rest (first calendar))) end]) ;; return the result
+    (<= (first (first calendar)) start) ;; in case the event overlaps the past event
+      (helper-get-available
+        (first (rest (first calendar)))
+        end
+        (rest calendar)
+        result)
+    :else (helper-get-available
+            (first (rest (first calendar))) ;; new start hour
+            end
+            (rest calendar) ;; new calendar
+            (my-append result [start (first (first calendar))])))) ;; new result
+
+(defn get-available [start end calendar]
+  (helper-get-available start end calendar []))
+
 ;; get-availability: {{{:calendar vector, :day-starts string, :day-ends string}}} -> map
 (defn get-availability [{{{:keys [calendar day-starts day-ends]} :body} :parameters}]
+  "Main function to get availability"
   {:status 200
-   :body {:available (my-map (fn [event] (my-map to-decimal-hour event)) calendar)}})
+   :body {:available (get-available
+                        (parse-int day-starts)
+                        (parse-int day-ends)
+                        (my-map (fn [event] (my-map to-decimal-hour event)) calendar))}})
