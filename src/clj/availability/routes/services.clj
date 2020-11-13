d091bde25f2fca18fae1a988f5eb48c77d2b2bb0
Topiltzin Hernández Mares <topiltzin.hernandez@wizeline.com>
2020-11-12 18:46:42 -0600

    Remove unused endpoints

--- src/clj/availability/routes/services.clj
+++ src/clj/availability/routes/services.clj
@@ -35,65 +35,10 @@
                  ;; multipart
                  multipart/multipart-middleware]}
 
-   ;; swagger documentation
-   ["" {:no-doc true
-        :swagger {:info {:title "my-api"
-                         :description "https://cljdoc.org/d/metosin/reitit"}}}
-
-    ["/swagger.json"
-     {:get (swagger/create-swagger-handler)}]
-
-    ["/api-docs/*"
-     {:get (swagger-ui/create-swagger-ui-handler
-             {:url "/api/swagger.json"
-              :config {:validator-url nil}})}]]
-
-   ["/ping"
-    {:get (constantly (ok {:message "pong"}))}]
-
    ["/availability"
     {:post {:summary "gets the availability of a given calendar"
            :parameters {:body {:calendar vector?
                                :day-starts string?
                                :day-ends string?}}
            :responses {200 {:body {:available seq?}}}
-           :handler get-availability}}]
-
-   ["/math"
-    {:swagger {:tags ["math"]}}
-
-    ["/plus"
-     {:get {:summary "plus with spec query parameters"
-            :parameters {:query {:x int?, :y int?}}
-            :responses {200 {:body {:total pos-int?}}}
-            :handler (fn [{{{:keys [x y]} :query} :parameters}]
-                       {:status 200
-                        :body {:total (+ x y)}})}
-      :post {:summary "plus with spec body parameters"
-             :parameters {:body {:x int?, :y int?}}
-             :responses {200 {:body {:total pos-int?}}}
-             :handler (fn [{{{:keys [x y]} :body} :parameters}]
-                        {:status 200
-                         :body {:total (+ x y)}})}}]]
-
-   ["/files"
-    {:swagger {:tags ["files"]}}
-
-    ["/upload"
-     {:post {:summary "upload a file"
-             :parameters {:multipart {:file multipart/temp-file-part}}
-             :responses {200 {:body {:name string?, :size int?}}}
-             :handler (fn [{{{:keys [file]} :multipart} :parameters}]
-                        {:status 200
-                         :body {:name (:filename file)
-                                :size (:size file)}})}}]
-
-    ["/download"
-     {:get {:summary "downloads a file"
-            :swagger {:produces ["image/png"]}
-            :handler (fn [_]
-                       {:status 200
-                        :headers {"Content-Type" "image/png"}
-                        :body (-> "public/img/warning_clojure.png"
-                                  (io/resource)
-                                  (io/input-stream))})}}]]])
+           :handler get-availability}}]])
