3eefab68fc692858a833f62c5cb64afcd5afdebe
Topiltzin Hernández Mares <topiltzin.hernandez@wizeline.com>
2020-11-21 22:37:34 -0600

    Add basic initial response

--- webapp/src/components/App/App.js
+++ webapp/src/components/App/App.js
@@ -1,7 +1,27 @@
+import { useState } from 'react';
+import axios from 'axios';
+import { AppContainer } from './App.styled';
+
+const URL = "http://localhost:8080/api/availability"
+
 const App = () => {
+  const [calendar, setCalendar] = useState([["10:30", "11:30"]]);
+  const [dayStarts, setDayStarts] = useState("08:00");
+  const [dayEnds, setDayEnds] = useState("18:00");
+
+  const getAvailability = async () => {
+    const response = await axios.post(URL, {
+      "day-starts": dayStarts,
+      "day-ends": dayEnds,
+      calendar: calendar
+    })
+    console.log(response);
+  }
+
   return (
-    <div className="App">
-    </div>
+    <AppContainer>
+      <button onClick={() => getAvailability()}>Get</button>
+    </AppContainer>
   );
 }
 
---
+++ webapp/src/components/App/App.styled.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const AppContainer = styled.section`
+  position: relative;
+  background-color: #222526;
+  width: 100vw;
+  height: 100vh;
+`;
